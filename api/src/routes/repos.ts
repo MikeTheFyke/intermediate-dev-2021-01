100114844c130593db65f63ec5a110b84cb83148
MikeTheFyke <mikefyke@hotmail.com>
2021-01-24 16:02:52 -0500

    Displaying only fork is false internalUserRepos

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,12 +1,35 @@
 import { Router, Request, Response } from 'express';
 const path = require('path');
-
-var internalUserRepos = require (path.resolve('data/repos.json'));
+const request = require('request');
 
 export const repos = Router();
 
+var internalUserRepos = require (path.resolve('data/repos.json'));
+
+var finalUserRepos = [];
+
+internalUserRepos.forEach (function(a) {
+  if(a.fork === false){
+    console.log("success")
+    finalUserRepos.push(a)
+  }
+  console.log(finalUserRepos)
+});
+
+
+// const userRequest = (accessToken) => request ('https://api.github.com/users/silverorange/repos',(error, response, body) => {
+//     if (error){
+//       console.log("Error has occured")
+//     } else {
+//       var externalUserRepos = body;
+//       console.log(externalUserRepos);
+//     }
+// });
+
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
-  
-  res.json(internalUserRepos);
-});
+
+  // userRequest();
+
+  res.json(finalUserRepos);
+})
